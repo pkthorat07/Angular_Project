fe78e0e0512fa43debcb39d045bc3db438296201
pkthorat07 <pavankumarthorat4@gmail.com>
2023-05-28 00:37:20 +0530

    update new files

--- testing4/src/app/app.module.ts
+++ testing4/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
