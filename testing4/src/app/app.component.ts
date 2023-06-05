454ef812fc445020d7fc04ecc474a50dfed7d24a
pkthorat07 <pavankumarthorat4@gmail.com>
2023-06-05 22:26:49 +0530

    on my way

--- testing4/src/app/app.component.ts
+++ testing4/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testing4';
+amount : number=5000;
+panNo :any="any@75" ;
+undef:any;
+studentname="Pavan";
+studmarks=107;
+value=true;
+myname="PavanThorat";
+gender="Male";
+city="Mumbai";
+
+submit(){
+  this.city="testing part";
+}
 }
